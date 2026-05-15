72f40363405af46da863f158c2c978a1ae5e06f5
GePuWrTs <gerd.puf@gmx.de>
2026-05-15 19:41:00 +0200

    For CT60 Focus ok & kexboard not visible

--- View/RackIncoming.xaml.cs
+++ View/RackIncoming.xaml.cs
@@ -1,28 +1,82 @@
-//using Java.Lang;
-using System.Reflection;
+#if ANDROID
+using Android.Content;
+using Android.Views.InputMethods;
+#endif
+
 
 namespace PblMauiShipment.View;
 
-public partial class RackIncoming : ContentPage {
-  RackScanIncomingViewModel _viewIncomingModel;
+public partial class RackIncoming : ContentPage
+{
+    RackScanIncomingViewModel _viewIncomingModel;
 
-  public RackIncoming(RackScanIncomingViewModel rackSannsIncomingViewModel) {
-    InitializeComponent();
-    Title = Properties.Resources.RackIncomming;
-    BindingContext = _viewIncomingModel = rackSannsIncomingViewModel;
-    if (_viewIncomingModel.DeviceModel == "CT60") {
-      lightGrid.IsVisible= false;
-      scannButton.IsVisible= false;
+    public RackIncoming(RackScanIncomingViewModel rackSannsIncomingViewModel)
+    {
+        InitializeComponent();
+        Title = Properties.Resources.RackIncomming;
+        BindingContext = _viewIncomingModel = rackSannsIncomingViewModel;
+        if (_viewIncomingModel.DeviceModel == "CT60")
+        {
+            lightGrid.IsVisible = false;
+            scannButton.IsVisible = false;
+        }
     }
-  }
 
-
-  private void barcodeIncomming_Completed(object sender, EventArgs e) {
-    if (barcode_incomming.Text != string.Empty) {
-      _viewIncomingModel.ZxingBarcodeStrIncomming = barcode_incomming.Text;
-      barcode_incomming.Text = string.Empty;
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+        await FocusBarcodeInputForScannerAsync();
+        Dispatcher.DispatchDelayed(TimeSpan.FromMilliseconds(250), async () => await FocusBarcodeInputForScannerAsync());
     }
-        barcode_incomming.Unfocus();
+
+    private async void barcodeIncomming_Completed(object sender, EventArgs e)
+    {
+        if (barcode_incomming.Text != string.Empty)
+        {
+            _viewIncomingModel.ZxingBarcodeStrIncomming = barcode_incomming.Text;
+            barcode_incomming.Text = string.Empty;
+        }
+        await FocusBarcodeInputForScannerAsync();
+    }
+
+    private async Task FocusBarcodeInputForScannerAsync()
+    {
+        bool suppressSoftKeyboard = _viewIncomingModel.DeviceModel == "CT60";
+
+#if ANDROID
+        var editText = barcode_incomming.Handler?.PlatformView as Android.Widget.EditText;
+        if (suppressSoftKeyboard && editText != null)
+        {
+            editText.ShowSoftInputOnFocus = false;
+        }
+#endif
+
+        barcode_incomming.Focus();
+
+#if ANDROID
+        if (editText != null)
+        {
+            editText.FocusableInTouchMode = true;
+            editText.RequestFocus();
+        }
+#endif
+
+        await Task.Delay(100);
+
+        if (!suppressSoftKeyboard)
+        {
+            return;
+        }
+
+#if ANDROID
+        if (editText != null)
+        {
+            var inputMethodManager = Microsoft.Maui.ApplicationModel.Platform.CurrentActivity?.GetSystemService(Context.InputMethodService) as InputMethodManager;
+            inputMethodManager?.HideSoftInputFromWindow(editText.WindowToken, HideSoftInputFlags.None);
+        }
+#else
+    await barcode_incomming.HideSoftInputAsync(CancellationToken.None);
+#endif
     }
 
     private void DeleteRackScannIncomming_Clicked(object sender, EventArgs e)
@@ -33,21 +87,26 @@ public partial class RackIncoming : ContentPage {
         }
     }
 
-    private async void FlashlightSwitchIncomming_Toggled(object sender, ToggledEventArgs e) {
-    try {
-      if (FlashlightSwitchIncomming.IsToggled)
-        await Flashlight.Default.TurnOnAsync();
-      else
-        await Flashlight.Default.TurnOffAsync();
+    private async void FlashlightSwitchIncomming_Toggled(object sender, ToggledEventArgs e)
+    {
+        try
+        {
+            if (FlashlightSwitchIncomming.IsToggled)
+                await Flashlight.Default.TurnOnAsync();
+            else
+                await Flashlight.Default.TurnOffAsync();
+        }
+        catch (FeatureNotSupportedException ex)
+        {
+            // Handle not supported on device exception
+        }
+        catch (PermissionException ex)
+        {
+            // Handle permission exception
+        }
+        catch (Exception ex)
+        {
+            // Unable to turn on/off flashlight
+        }
     }
-    catch (FeatureNotSupportedException ex) {
-      // Handle not supported on device exception
-    }
-    catch (PermissionException ex) {
-      // Handle permission exception
-    }
-    catch (Exception ex) {
-      // Unable to turn on/off flashlight
-    }
-  }
 }
--- View/RackRegister.xaml.cs
+++ View/RackRegister.xaml.cs
@@ -1,25 +1,82 @@
+#if ANDROID
+using Android.Content;
+using Android.Views.InputMethods;
+#endif
+
 namespace PblMauiShipment.View;
 
-public partial class RackRegister : ContentPage {
-  RackScanRegisterViewModel _viewRegisterModel;
+public partial class RackRegister : ContentPage
+{
+    RackScanRegisterViewModel _viewRegisterModel;
 
-  public RackRegister(RackScanRegisterViewModel rackSannsRegisterViewModel) {
-    InitializeComponent();
-    Title = Properties.Resources.RackRegistation;
-    BindingContext = _viewRegisterModel = rackSannsRegisterViewModel;
-    if (_viewRegisterModel.DeviceModel == "CT60") {
-      lightGrid.IsVisible = false;
-      scannButton.IsVisible = false;
+    public RackRegister(RackScanRegisterViewModel rackSannsRegisterViewModel)
+    {
+        InitializeComponent();
+        Title = Properties.Resources.RackRegistation;
+        BindingContext = _viewRegisterModel = rackSannsRegisterViewModel;
+        if (_viewRegisterModel.DeviceModel == "CT60")
+        {
+            lightGrid.IsVisible = false;
+            scannButton.IsVisible = false;
+        }
     }
-  }
 
-  private void barcode_register_Completed(object sender, EventArgs e) {
-    if (barcode_register.Text != string.Empty) {
-      _viewRegisterModel.ZxingBarcodeStrRegister = barcode_register.Text;
-      barcode_register.Text = string.Empty;
+    protected override async void OnAppearing()
+    {
+        base.OnAppearing();
+        await FocusBarcodeInputForScannerAsync();
+        Dispatcher.DispatchDelayed(TimeSpan.FromMilliseconds(250), async () => await FocusBarcodeInputForScannerAsync());
     }
-    barcode_register.Unfocus();
-  }
+
+    private async void barcode_register_Completed(object sender, EventArgs e)
+    {
+        if (barcode_register.Text != string.Empty)
+        {
+            _viewRegisterModel.ZxingBarcodeStrRegister = barcode_register.Text;
+            barcode_register.Text = string.Empty;
+        }
+        await FocusBarcodeInputForScannerAsync();
+    }
+
+    private async Task FocusBarcodeInputForScannerAsync()
+    {
+        bool suppressSoftKeyboard = _viewRegisterModel.DeviceModel == "CT60";
+
+#if ANDROID
+        var editText = barcode_register.Handler?.PlatformView as Android.Widget.EditText;
+        if (suppressSoftKeyboard && editText != null)
+        {
+            editText.ShowSoftInputOnFocus = false;
+        }
+#endif
+
+        barcode_register.Focus();
+
+#if ANDROID
+        if (editText != null)
+        {
+            editText.FocusableInTouchMode = true;
+            editText.RequestFocus();
+        }
+#endif
+
+        await Task.Delay(100);
+
+        if (!suppressSoftKeyboard)
+        {
+            return;
+        }
+
+#if ANDROID
+        if (editText != null)
+        {
+            var inputMethodManager = Microsoft.Maui.ApplicationModel.Platform.CurrentActivity?.GetSystemService(Context.InputMethodService) as InputMethodManager;
+            inputMethodManager?.HideSoftInputFromWindow(editText.WindowToken, HideSoftInputFlags.None);
+        }
+#else
+    await barcode_register.HideSoftInputAsync(CancellationToken.None);
+#endif
+    }
 
     private void DeleteRackScannRegister_Clicked(object sender, EventArgs e)
     {
@@ -29,35 +86,42 @@ public partial class RackRegister : ContentPage {
         }
     }
 
-    private async void FlashlightSwitchRegister_Toggled(object sender, ToggledEventArgs e) {
-    try {
-      if (FlashlightSwitchRegister.IsToggled)
-        await Flashlight.Default.TurnOnAsync();
-      else
-        await Flashlight.Default.TurnOffAsync();
-    }
-    catch (FeatureNotSupportedException ex) {
-      // Handle not supported on device exception
-    }
-    catch (PermissionException ex) {
-      // Handle permission exception
-    }
-    catch (Exception ex) {
-      // Unable to turn on/off flashlight
+    private async void FlashlightSwitchRegister_Toggled(object sender, ToggledEventArgs e)
+    {
+        try
+        {
+            if (FlashlightSwitchRegister.IsToggled)
+                await Flashlight.Default.TurnOnAsync();
+            else
+                await Flashlight.Default.TurnOffAsync();
+        }
+        catch (FeatureNotSupportedException ex)
+        {
+            // Handle not supported on device exception
+        }
+        catch (PermissionException ex)
+        {
+            // Handle permission exception
+        }
+        catch (Exception ex)
+        {
+            // Unable to turn on/off flashlight
+        }
+
     }
 
-  }
+    private void rackOwner_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        //>SelectedIndexChanged="rackOwner_SelectedIndexChanged"
 
-  private void rackOwner_SelectedIndexChanged(object sender, EventArgs e) {
-    //>SelectedIndexChanged="rackOwner_SelectedIndexChanged"
+        var picker = (Picker)sender;
+        int selectedIndex = picker.SelectedIndex;
 
-    var picker = (Picker)sender;
-    int selectedIndex = picker.SelectedIndex;
-
-    if (selectedIndex != -1) {
-      var p = picker.ItemsSource[selectedIndex];
-      _viewRegisterModel.RackOwnerSelected = (RackOwner)p;
+        if (selectedIndex != -1)
+        {
+            var p = picker.ItemsSource[selectedIndex];
+            _viewRegisterModel.RackOwnerSelected = (RackOwner)p;
+        }
     }
-  }
 
 }
